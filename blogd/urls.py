9cb053d6565cbb22c8a59ebee06f91cad6f90977
Mustafaunsall <unsalmustafa541@gmail.com>
2021-06-18 00:50:13 +0300

    anasayfa blogların gösterimi yapıldı

--- blogd/urls.py
+++ blogd/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('home/', include('home.urls')), #home app urli tanıt
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('category/<int:id>/<slug:slug>/', views.category_blogs,name='category_blogs'),
 ]
 if settings.DEBUG: #new resimleri göstermek için yol
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -10,41 +10,66 @@ def index(request):
     settings = Settings.objects.get(pk=1)
     category = Category.objects.all()
     sliderdata = Blog.objects.all()[:5]
+    dayblogs = Blog.objects.all()[:6]
+    lastblogs = Blog.objects.all().order_by('-id')[:4]
+    randomblogs = Blog.objects.all().order_by('?')[:9]
+    populerblogs = Blog.objects.all()[:5]
     context = {'settings': settings,
-               'sliderdata':sliderdata,
-               'category':category
+               'sliderdata': sliderdata,
+               'dayblogs': dayblogs,
+               'category': category,
+               'lastblogs': lastblogs,
+               'randomblogs': randomblogs,
+               'populerblogs': populerblogs,
                }
     return render(request, 'index.html', context)
 
+
 def hakkimizda(request):
     settings = Settings.objects.get(pk=1)
+    category = Category.objects.all()
 
-    context = {'settings': settings}
+    context = {'settings': settings, 'category': category}
     return render(request, 'hakkimizda.html', context)
+
 
 def referanslarımız(request):
     settings = Settings.objects.get(pk=1)
+    category = Category.objects.all()
 
-    context = {'settings': settings,}
+    context = {'settings': settings, 'category': category}
     return render(request, 'referanslarımız.html', context)
 
+
 def iletisim(request):
-    if request.method =='POST': #form post edildi ise
+    if request.method == 'POST':  # form post edildi ise
         form = ContactFormu(request.POST)
-        if  form.is_valid():
-            data = ContactFormMessage() #model ile bağlantı kurma
-            data.name = form.cleaned_data['name']  #formdan bilgi al
+        if form.is_valid():
+            data = ContactFormMessage()  # model ile bağlantı kurma
+            data.name = form.cleaned_data['name']  # formdan bilgi al
             data.email = form.cleaned_data['email']
             data.subject = form.cleaned_data['subject']
             data.message = form.cleaned_data['message']
             data.ip = request.META.get('REMOTE_ADDR')
-            data.save() #veri tabanına kaydet
-            messages.success(request,"mesajınız başarı ile kaydedilmiştir")
+            data.save()  # veri tabanına kaydet
+            messages.success(request, "mesajınız başarı ile kaydedilmiştir")
             return HttpResponseRedirect('/iletisim')
 
-
-
+    category = Category.objects.all()
     settings = Settings.objects.get(pk=1)
-    form=ContactFormu()
-    context = {'settings': settings,'form':form}
+    form = ContactFormu()
+    context = {'settings': settings, 'form': form, 'category': category}
     return render(request, 'iletisim.html', context)
+
+
+def category_blogs(request,id,slug):
+    category = Category.objects.all()
+    categorydata = Category.objects.get(pk=id)
+    blogs = Blog.objects.filter(category_id=id)
+    context = {
+            'blogs': blogs,
+            'category': category,
+            'categorydata':categorydata,
+               }
+
+    return render(request, 'blogs.html', context)
